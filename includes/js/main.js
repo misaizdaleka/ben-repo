b224992c345a272d4525cbe42622b7ada7e3cdf5
misaizdaleka <milos.boskovic@gmail.com>
2013-04-09 16:44:24 +0200

    - Styling

--- includes/js/main.js
+++ includes/js/main.js
@@ -1,97 +1,82 @@
-var graph = {
-  day : null,
-  dayOfMonth : null,
-  week : null,
-  month : null,
-  percent : null,
-  format : null,
-  cellSize: null,
-
-  init: function() 
-  {
-    var width = 960,
-    height = 136;
-
-    var _this = this;
-
-    this.day        = d3.time.format("%w"),
-    this.dayOfMonth = d3.time.format("%d")
-    this.week       = d3.time.format("%U"),
-    this.month      = d3.time.format("%b"),
-    this.percent    = d3.format(".1%"),
-    this.format     = d3.time.format("%Y-%m-%d");
-
-    var color = d3.scale.quantize()
-        .domain([0, 100])
-        .range(d3.range(5).map(function(d) { return "q" + d + "-11"; }));
-
-    var svg = d3.select("body>div.main-container").selectAll('svg')
-        .data(d3.range(2013, 2014))
-      .enter().append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("class", "RdYlGn")
-      .append("g")
-        .attr("transform", "translate(" + ((width - _this.cellSize * 53) / 2) + "," + (height - _this.cellSize * 7 - 1) + ")");
-
-        console.log(svg);
-
-    svg.append("text")
-        .attr("transform", "translate(-6," + _this.cellSize * 3.5 + ")rotate(-90)")
-        .style("text-anchor", "middle")
-        .text(function(d) { return d; });
-
-    var rect = svg.selectAll(".day")
-        .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-        .enter().append("rect")
-        .attr("class", function(d, i) {
-              return "day "+color(bData[i].value);
-            })
-        .attr("width", _this.cellSize)
-        .attr("height", _this.cellSize)
-        .attr("x", function(d) { return _this.week(d) * _this.cellSize; })
-        .attr("y", function(d) { return _this.day(d) * _this.cellSize; })
-        .each(function(d) {
-           if (_this.day(d)*_this.cellSize == 0 && _this.dayOfMonth(d)<=7) {
-              svg.append("text")
-                  .text(_this.month(d))
-                  .attr("x", function() {
-                    return _this.week(d) * _this.cellSize + 5;
-                  })
-                  .attr("y", -5);
-           }
-        })
-        .datum(_this.format);
-
-
-      rect.append("title")
-        .text(function(d, i) { return d+", "+bData[i].value; });
-
-     /* svg.selectAll(".month")
-        .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-       .enter().append("path")
-        .attr("class", "month")
-        .attr("d", _this.monthPath);*/
-
-  },
-
-  monthPath: function(t0)
-  {
-    var _this = this;
-
-    console.log(this.day);
-
-     var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
-          d0 = _this.day(t0), w0 = _this.week(t0),
-          d1 = _this.day(t1), w1 = _this.week(t1);
-      return "M" + (w0 + 1) * _this.cellSize + "," + d0 * _this.cellSize
-          + "H" + w0 * _this.cellSize + "V" + 7 * _this.cellSize
-          + "H" + w1 * _this.cellSize + "V" + (d1 + 1) * _this.cellSize
-          + "H" + (w1 + 1) * _this.cellSize + "V" + 0
-          + "H" + (w0 + 1) * _this.cellSize + "Z";
-  }
-}
 
 jQuery(document).ready(function($) {
-  graph.init();
+  function monthPath(t0) {
+    var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+        d0 = +day(t0), w0 = +week(t0),
+        d1 = +day(t1), w1 = +week(t1);
+    return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
+        + "H" + w0 * cellSize + "V" + 7 * cellSize
+        + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
+        + "H" + (w1 + 1) * cellSize + "V" + 0
+        + "H" + (w0 + 1) * cellSize + "Z";
+  }
+
+  var width = 800,
+      height = 136,
+      cellSize = 15; // cell size
+
+  var day = d3.time.format("%w"),
+      dayOfMonth = d3.time.format("%d")
+      week = d3.time.format("%U"),
+      month = d3.time.format("%b"),
+      percent = d3.format(".1%"),
+      format = d3.time.format("%Y-%m-%d");
+
+  var color = d3.scale.quantize()
+      .domain([0, 100])
+      .range(d3.range(5).map(function(d) { return "q" + d + "-11"; }));
+
+  var svg = d3.select("body>div.main-container").append("svg")
+      .data(d3.range(2013, 2014))
+    .enter().append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("class", "RdYlGn")
+    .append("g")
+      .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+
+  svg.append("text")
+      .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+      .attr('class', 'text-month')
+      .style("text-anchor", "middle")
+      .text(function(d) { return d; });
+
+  var rect = svg.selectAll(".day")
+      .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+      .enter().append("rect")
+      .attr("class", function(d, i) {
+            return "day "+color(bData[i].value);
+          })
+      .attr("width", cellSize-2)
+      .attr("height", cellSize-2)
+      .attr("x", function(d) { return week(d) * cellSize + 1; })
+      .attr("y", function(d) { return day(d) * cellSize + 1; })
+      .each(function(d) {
+         if (day(d)*cellSize == 0 && dayOfMonth(d)<=7) {
+            svg.append("text")
+                .text(month(d))
+                .attr('class', 'text-month')
+                .attr("x", function() {
+                  return week(d) * cellSize + 5;
+                })
+                .attr("y", -5);
+         }
+      })
+      .datum(format);
+
+
+    rect.append("title")
+      .text(function(d, i) { return d+", "+bData[i].value; });
+
+    svg.selectAll(".month")
+      .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+     .enter().append("path")
+      .attr("class", "month")
+      .attr("d", monthPath);
+
+    $("div.main-container").append($('svg'));
+
 });
+
+
+
